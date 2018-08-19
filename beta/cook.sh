6105bb0760fde72c88372dff808d41b46c065b0c
luandro <luandro@gmail.com>
2018-08-19 09:56:37 -0300

    added sciprts and script to prepare new server for cooking

---
+++ beta/cook.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/libremesh/lime-sdk.git
+cd lime-sdk
+sudo docker build -t cooker .
+sudo docker run -v "$(pwd)":/app cooker -f
+./snippets/regdbtz.sh
+sudo docker run -v "$(pwd)":/app cooker -b ar71xx/generic --remote
+sudo docker run -v "$(pwd)":/app cooker -c ar71xx/generic --profile=tl-wdr3500-v1 --community=moinho/comun --remote J=1 V=s
+sudo docker run -v "$(pwd)":/app cooker -c ar71xx/generic --profile=tl-wdr3600-v1 --community=moinho/comun --remote J=1 V=s
+sudo docker run -v "$(pwd)":/app cooker -c ar71xx/generic --profile=tl-wr1043nd-v3 --community=moinho/comun --remote J=1 V=s
+curl -F "file=@test.txt" https://file.io
