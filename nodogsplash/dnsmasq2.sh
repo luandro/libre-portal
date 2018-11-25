d4df527f931f3ab8cc624446a1e709f7233d7c1d
luandro <luandro@gmail.com>
2018-11-25 14:05:34 -0200

    fixed dnsmasq2 script

--- nodogsplash/dnsmasq2.sh
+++ nodogsplash/dnsmasq2.sh
@@ -3,12 +3,11 @@
 iptables -L -vn -t mangle
 ps | grep dns
 cp /var/etc/dnsmasq.conf.cfg02411c /etc/dnsmasq2
-vim /etc/dnsmasq2
-address=10.5.0.1
+gwip=$(uci get network.lan.ipaddr)
 dnsmasq2=$(cat <<EOF
   # auto-generated config file from /etc/config/dhcp
   domain=lan
-  address=/#/$address
+  address=/#/$gwip
   addn-hosts=/tmp/hosts
   user=dnsmasq
   group=dnsmasq
@@ -16,17 +15,19 @@ dnsmasq2=$(cat <<EOF
 EOF
 )
 
-sed -i '/FirewallRule allow udp port 53/c\FirewallRule allow udp port 53530' /etc/nodogsplash/nodogsplash.conf
-sed -i '/FirewallRule allow tcp port 53/c\FirewallRule allow tcp port 53530' /etc/nodogsplash/nodogsplash.conf
+echo $dnsmasq2 >> /etc/dnsmasq2
 
-vim /etc/rc.local
-rc-local=$(cat <<EOF
+sed -i '/#FirewallRule allow udp port 53/c\FirewallRule allow udp port 53530' /etc/nodogsplash/nodogsplash.conf
+sed -i '/#FirewallRule allow tcp port 53/c\FirewallRule allow tcp port 53530' /etc/nodogsplash/nodogsplash.conf
+
+rclocal=$(cat <<EOF
   # the system init finished. By default this file does nothing.
   /usr/sbin/dnsmasq -C /etc/dnsmasq2
 
   ((sleep 60; \
-    iptables -t nat -I ndsOUT 3 -p udp -m udp --dport 53 -j DNAT --to-destination $address:53530 ; \
-    iptables -t nat -I ndsOUT 4 -p tcp -m tcp --dport 53 -j DNAT --to-destination $address:53530 )&)
+    iptables -t nat -I ndsOUT 3 -p udp -m udp --dport 53 -j DNAT --to-destination $gwip:53530 ; \
+    iptables -t nat -I ndsOUT 4 -p tcp -m tcp --dport 53 -j DNAT --to-destination $gwip:53530 )&)
   exit 0
 EOF
 )
+echo $rclocal >> /etc/rc.local
--- nodogsplash/post-install.sh
+++ nodogsplash/post-install.sh
@@ -2,5 +2,6 @@
 
 scp ./nodogsplash/splash.html root@thisnode.info:/etc/nodogsplash/htdocs/
 scp ./nodogsplash/img.jpg root@thisnode.info:/www/nds/
-echo "Updating splash screen"
-mv /etc/nodogsplash/htdocs/splash-vale.html /etc/nodogsplash/htdocs/splash.html
+/etc/init.d/nodogsplash restart
+# echo "Updating splash screen"
+# mv /etc/nodogsplash/htdocs/splash-vale.html /etc/nodogsplash/htdocs/splash.html
